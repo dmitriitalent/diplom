953f07bb9de53662a7e882a3ad32ecb68ecddff7
Dmitriy Gavshin <dmitriitalent@gmail.com>
2026-01-08 01:18:20 +0300

    Add UiImage

---
+++ app/components/ui/UiImage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps({
+	src: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+
+<template>
+	<div :class="$style.wrapper">
+		<img :class="$style.image" :src="props.src" />
+	</div>
+</template>
+
+<style module lang="scss">
+.wrapper {
+	height: 100%;
+	width: 100%;
+
+	.image {
+		display: block;
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+}
+</style>
